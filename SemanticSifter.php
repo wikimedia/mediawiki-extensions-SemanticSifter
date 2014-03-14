2ca71d28c39a1e14bb62ff85274fc6d08bc60dd3
Kim Eik <kim@heldig.org>
2014-03-14 11:08:48 +0100

    Added composer
    Change-Id: Ib1e28aa7e3ae4775d6d7cb9e1abef98749394579

--- SemanticSifter.php
+++ SemanticSifter.php
@@ -14,67 +14,33 @@ if ( !defined( 'ParamProcessor_VERSION' ) ) {
 	die( 1 );
 }
 
-function endsWith($haystack, $needle){
-	return $needle === "" || substr($haystack, -strlen($needle)) === $needle;
-}
+global $wgExtensionCredits,$wgHooks,$wgExtensionMessagesFiles,$wgResourceModules,$wgAjaxExportList;
 
-spl_autoload_register( function ( $className ) {
-	$className = ltrim( $className, '\\' );
-	$fileName = '';
-	$namespace = '';
-	$lastNsPos = strripos( $className, '\\');
+//credits
+$wgExtensionCredits['parserhook'][] = array(
+	'path' => __FILE__,
+	'name' => 'Semantic Sifter',
+	'descriptionmsg' => 'semanticsifter-desc',
+	'version' => '0.1',
+	'author' => 'Kim Eik',
+);
 
-	if ( $lastNsPos ) {
-		$namespace = substr( $className, 0, $lastNsPos );
-		$className = substr( $className, $lastNsPos + 1 );
-		$fileName  = str_replace( '\\', '/', $namespace ) . '/';
-	}
+//ajax
+$wgAjaxExportList[] = 'SemanticSifter\API\API::filter';
 
-	$fileName .= str_replace( '_', '/', $className ) . '.php';
+//hooks
+$wgHooks['ParserFirstCallInit'][] = 'SemanticSifter\SemanticSifterHooks::parserFunctionInit';
+$wgHooks['UnitTestsList'][] = 'SemanticSifter\SemanticSifterHooks::unitTestsInit';
 
-	$namespaceSegments = explode( '\\', $namespace );
+//i18n
+$wgExtensionMessagesFiles['SemanticSifter'] = dirname( __FILE__ ) . '/SemanticSifter.i18n.php';
+$wgExtensionMessagesFiles['SemanticSifterMagic'] = dirname( __FILE__ ) . '/SemanticSifter.i18n.magic.php';
 
-	if ( array_shift($namespaceSegments) === 'SemanticSifter' ) {
-		$fileName = substr($fileName,$lastNsPos);
-		$namespaceSegments = array_map('strtolower', $namespaceSegments);
-
-		if(endsWith($fileName,'Test.php')){
-			require_once (__DIR__ . '/src/test/' . join('/',$namespaceSegments) . $fileName);
-		}else{
-			require_once (__DIR__ . '/src/main/' . join('/',$namespaceSegments) . $fileName);
-		}
-	}
-} );
-
-call_user_func( function() {
-	global $wgExtensionCredits,$wgHooks,$wgExtensionMessagesFiles,$wgResourceModules,$wgAjaxExportList;
-
-	//credits
-	$wgExtensionCredits['parserhook'][] = array(
-		'path' => __FILE__,
-		'name' => 'Semantic Sifter',
-		'descriptionmsg' => 'semanticsifter-desc',
-		'version' => '0.1',
-		'author' => 'Kim Eik',
-	);
-
-	//ajax
-	$wgAjaxExportList[] = 'SemanticSifter\API\API::filter';
-
-	//hooks
-	$wgHooks['ParserFirstCallInit'][] = 'SemanticSifter\SemanticSifterHooks::parserFunctionInit';
-	$wgHooks['UnitTestsList'][] = 'SemanticSifter\SemanticSifterHooks::unitTestsInit';
-
-	//i18n
-	$wgExtensionMessagesFiles['SemanticSifter'] = dirname( __FILE__ ) . '/SemanticSifter.i18n.php';
-	$wgExtensionMessagesFiles['SemanticSifterMagic'] = dirname( __FILE__ ) . '/SemanticSifter.i18n.magic.php';
-
-	//resource modules
-	$wgResourceModules['ext.semanticsifter'] = array(
-		'localBasePath' => dirname( __FILE__ ),
-		'remoteExtPath' => 'SemanticSifter',
-		'dependencies' => array( 'jquery.chosen','mediawiki.Uri' ),
-		'scripts' => '/resources/main/js/ext.semanticsifter.js',
-		'styles' => '/resources/main/css/ext.semanticsifter.css'
-	);
-});
+//resource modules
+$wgResourceModules['ext.semanticsifter'] = array(
+	'localBasePath' => dirname( __FILE__ ),
+	'remoteExtPath' => 'SemanticSifter',
+	'dependencies' => array( 'jquery.chosen','mediawiki.Uri' ),
+	'scripts' => '/resources/main/js/ext.semanticsifter.js',
+	'styles' => '/resources/main/css/ext.semanticsifter.css'
+);
