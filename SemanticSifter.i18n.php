ce7d882b5e273288f253cad8d30795f663939206
Raimond Spekking <raimond.spekking@gmail.com>
2014-03-19 12:36:56 +0100

    Consistency tweaks in preparation for adding extension to translatewiki.net
    * Add header
    * Rephrase description message
    * Tweak to message documentation
    
    Change-Id: I63b16c7be4e533328d1495f494ecf98c16f1c782

--- SemanticSifter.i18n.php
+++ SemanticSifter.i18n.php
@@ -1,14 +1,27 @@
 <?php
+/**
+ * Internationalization file for Semantic Sifter extension.
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
 $messages = array();
 
+/** English
+ * @author Kim Eik
+ */
 $messages['en'] = array(
-	'semanticsifter-desc' => 'An extension which allows you to sift through semantic data',
+	'semanticsifter-desc' => 'Allows to sift through semantic data and displays the result in a filterable view',
 	'semanticsifter-button-apply-filter' => 'Apply filter',
 	'semanticsifter-message-siftlink-params-error' => 'Missing parameters to siftlink parser function.'
 );
 
+/** Message documentation
+ * @author Kim Eik
+ */
 $messages['qqq'] = array(
-	'semanticsifter-desc' => 'Extension description',
+	'semanticsifter-desc' => '{{desc|name=Semantic Sifter|url=https://www.mediawiki.org/wiki/Extension:SemanticSifter}}',
 	'semanticsifter-button-apply-filter' => 'Button which applies filter',
 	'semanticsifter-message-siftlink-params-error' => 'Message displayed  when error occurs in parameter handling of #siftlink parser function'
 
